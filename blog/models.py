e1f05c70272fcf77cb7aa107a51b9c2018450038
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2018-10-24 17:36:32 -0300

    Modifica los posts agregando nuevos campos y permite subir fotos

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,18 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    CONDITION = (
+        ('Rescatado', 'Rescatado'),
+        ('Disponible', 'Disponible'),
+        ('Adoptado', 'Adoptado'),
+    )
+
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField()
+    condition = models.TextField(choices=CONDITION, default='Rescatado')
     picture = models.ImageField(
         upload_to='user_pics', default='default.jpg')
 
     def __str__(self):
-        return self.title
+        return self.name

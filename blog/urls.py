c366beea3dcfb23ed683d2d738aa14ef379cbdb3
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2018-10-26 02:15:04 -0300

    Añade y cambia vista del CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
+from .views import (PostListView, PostCreateView,
+                    PostUpdateView, PostDeleteView)
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -7,3 +9,38 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html/'
+    context_object_name = 'posts'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['name', 'breed', 'description', 'condition', 'picture']
+
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        if self.request.user.has_perm('post.change_post'):
+            return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['name', 'breed', 'description', 'condition', 'picture']
+
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        if self.request.user.has_perm('post.change_post'):
+            return super().form_valid(form)
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
